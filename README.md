1830bb827fd8827739169624a29369cba86c0643
girlealvarez <gialvarezg@unal.edu.co>
2024-06-17 09:42:27 -0500

    junio 17

--- README.md
+++
@@ -1,3 +0,0 @@
-# UNALWater Final Project
-
-![image](https://github.com/ysgiraldo/UNALWater_BD/assets/160282091/f781401e-870f-4a76-b9fb-c1c80f6ebbd0)
--- import_hdfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sqoop import \
-    --connect jdbc:mysql://localhost:3306/demo_db \
-    --username sqoop \
-    --password secret \
-    --table customers \
-    --target-dir /tmp/ \
-    --m 1
